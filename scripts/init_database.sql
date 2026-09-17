c8cd88c49b08395e2a6edf6a50178fd869d51912
Shreyash-lunge <shreyashlunge23@gmail.com>
2026-09-18 02:06:33 +0530

    Create init_database.sql for DataWarehouse setup

---
+++ scripts/init_database.sql
@@ -0,0 +1,17 @@
+/*
+create database and schema
+*/
+
+
+USE master;
+GO
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
